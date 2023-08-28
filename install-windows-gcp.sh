5ecb4fc5883b367e6a195e84578224250908be7e
Xuan Son Nguyen <thichthat@gmail.com>
2023-08-28 17:59:22 +0200

    Update install-windows-gcp.sh

--- install-windows-gcp.sh
+++ install-windows-gcp.sh
@@ -17,6 +17,17 @@ echo ""
 
 wget -O windows.raw.gz $IMAGE_URL
 
+# get all block devices, sort by SIZE to get the biggest device
+DESTINATION_DEVICE="$(lsblk -x SIZE -o NAME,SIZE | tail -n1 | cut -d ' ' -f 1)"
+
+# check if the disk already has multiple partitions
+NB_PARTITIONS=$(lsblk | grep -c "$DESTINATION_DEVICE")
+if [ "$NB_PARTITIONS" -gt 1 ]; then
+    echo "ERROR: Device $DESTINATION_DEVICE already has some partitions."
+    echo "Please make sure that $DESTINATION_DEVICE is an empty disk"
+    exit 1
+fi
+
 echo ""
 echo ""
 echo "    COPYING IMAGE FILE... (may take about 5 minutes)"
@@ -24,12 +35,9 @@ echo "    Do NOT close this terminal until it finishes"
 echo ""
 echo ""
 
-# get all block devices, sort by SIZE to get the biggest device
-DESTINATION_DEVICE="/dev/"$(lsblk -x SIZE -o NAME,SIZE | tail -n1 | cut -d ' ' -f 1)
-
 # then, use dd to copy image
 echo "Destination device is $DESTINATION_DEVICE"
-pigz -dc ./windows.raw.gz | sudo dd of=$DESTINATION_DEVICE bs=4M
+pigz -dc ./windows.raw.gz | sudo dd of="/dev/$DESTINATION_DEVICE" bs=4M
 
 echo ""
 echo ""
